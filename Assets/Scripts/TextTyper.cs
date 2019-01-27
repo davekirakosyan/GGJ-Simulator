d38070f6cd5c6d97f61508ff8f1d3bd9964aa798
davekirakosyan <dav.kyan@gmail.com>
2019-01-27 14:17:44 +0400

    some changes

---
+++ Assets/Scripts/TextTyper.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextTyper : MonoBehaviour
+{
+    private int count;
+    public Text text;
+    private string fullText = "void FixedUpdate()\n    {\n        float h = Input.GetAxis();\n        if (h * rb2d.velocity.x < maxSpeed)\n            rb2d.AddForce(Vector2.right * h * moveForce);\n        if (Mathf.Abs(rb2d.velocity.x) > maxSpeed)\n            rb2d.velocity = new Vector2(Mathf.Sign(rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);\n        if (h > 0 && !facingRight)";
+        
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown && count<345)
+        {
+            text.text+=char.ToString(fullText[count]);
+            count++;
+        }
+    }
+}
